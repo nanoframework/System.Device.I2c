0166061fac2b9c84bb2b19c27bd5031fd18500ed
Laurent Ellerbach <laurelle@microsoft.com>
2020-10-02 12:28:48 +0200

    Initial commit adding System.Device.I2c managed implementation (#1)

---
+++ System.Device.I2c/I2cConnectionSettings.cs
@@ -0,0 +1,79 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Device.I2c
+{
+    /// <summary>
+    /// The connection settings of a device on an I2C bus.
+    /// </summary>
+    public sealed class I2cConnectionSettings
+    {
+
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private int _deviceAddress;
+
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private I2cBusSpeed _busSpeed;
+
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private I2cSharingMode _sharingMode;
+
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private int _busId;
+
+        private I2cConnectionSettings()
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="I2cConnectionSettings"/> class.
+        /// </summary>
+        /// <param name="busId">The bus ID the I2C device is connected to.</param>
+        /// <param name="deviceAddress">The bus address of the I2C device.</param>
+        public I2cConnectionSettings(int busId, int deviceAddress) : this(busId, deviceAddress, I2cBusSpeed.FastMode, I2cSharingMode.Shared)
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="I2cConnectionSettings"/> class.
+        /// </summary>
+        /// <param name="busId">The bus ID the I2C device is connected to.</param>
+        /// <param name="deviceAddress">The bus address of the I2C device.</param>
+        /// <param name="busSpeed">The bus speed of the I2C device.</param>
+        /// <param name="sharingMode">The sharing mode of the I2C device.</param>
+        public I2cConnectionSettings(int busId, int deviceAddress, I2cBusSpeed busSpeed, I2cSharingMode sharingMode)
+        {
+            _busId = busId;
+            _deviceAddress = deviceAddress;
+            _busSpeed = busSpeed;
+            _sharingMode = sharingMode;
+        }
+
+        internal I2cConnectionSettings(I2cConnectionSettings other)
+        {
+            _busId = other.BusId;
+            _deviceAddress = other.DeviceAddress;
+            _busSpeed = other.BusSpeed;
+            _sharingMode = other.SharingMode;
+        }
+
+        /// <summary>
+        /// The bus ID the I2C device is connected to.
+        /// </summary>
+        public int BusId { get => _busId; }
+
+        /// <summary>
+        /// The bus address of the I2C device.
+        /// </summary>
+        public int DeviceAddress { get => _deviceAddress; }
+
+        /// <summary>
+        /// The bus speed of the I2C device
+        /// </summary>
+        public I2cBusSpeed BusSpeed { get => _busSpeed; }
+
+        /// <summary>
+        /// Sharing mode of the I2C device
+        /// </summary>
+        public I2cSharingMode SharingMode { get => _sharingMode; }
+    }
+}
---
+++ System.Device.I2c/I2cDevice.cs
@@ -0,0 +1,139 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace System.Device.I2c
+{
+    /// <summary>
+    /// The communications channel to a device on an I2C bus.
+    /// </summary>
+    public class I2cDevice : IDisposable
+    {
+
+        private Windows.Devices.I2c.I2cDevice _device;
+        // For the ReadByte and WriteByte operations
+        private byte[] bufferSingleOperation = new byte[1];
+
+        /// <summary>
+        /// The connection settings of a device on an I2C bus. The connection settings are immutable after the device is created
+        /// so the object returned will be a clone of the settings object.
+        /// </summary>
+        public I2cConnectionSettings ConnectionSettings { get; }
+
+        /// <summary>
+        /// Reads a byte from the I2C device.
+        /// </summary>
+        /// <returns>A byte read from the I2C device.</returns>
+        public byte ReadByte()
+        {
+            _device.Read(bufferSingleOperation);
+            return bufferSingleOperation[0];
+        }
+
+        /// <summary>
+        /// Reads data from the I2C device.
+        /// </summary>
+        /// <param name="buffer">
+        /// The buffer to read the data from the I2C device.
+        /// The length of the buffer determines how much data to read from the I2C device.
+        /// </param>
+        public void Read(SpanByte buffer)
+        {
+            // This is allocating an intermediate buffer and then copy back the data to 
+            // the SpanByte. This is intend to be changed in a native implementation
+            byte[] toRead = new byte[buffer.Length];
+            _device.Read(toRead);
+            for (int i = 0; i < toRead.Length; i++)
+            {
+                buffer[i] = toRead[i];
+            }
+        }
+
+        /// <summary>
+        /// Writes a byte to the I2C device.
+        /// </summary>
+        /// <param name="value">The byte to be written to the I2C device.</param>
+        public void WriteByte(byte value)
+        {
+            bufferSingleOperation[0] = value;
+            _device.Write(bufferSingleOperation);
+        }
+
+        /// <summary>
+        /// Writes data to the I2C device.
+        /// </summary>
+        /// <param name="buffer">
+        /// The buffer that contains the data to be written to the I2C device.
+        /// The data should not include the I2C device address.
+        /// </param>
+        public void Write(SpanByte buffer)
+        {
+            // This is allocating an intermediate buffer using the buffer of 
+            // the SpanByte. This is intend to be changed in a native implementation
+            _device.Write(buffer.ToArray());
+        }
+
+        /// <summary>
+        /// Performs an atomic operation to write data to and then read data from the I2C bus on which the device is connected,
+        /// and sends a restart condition between the write and read operations.
+        /// </summary>
+        /// <param name="writeBuffer">
+        /// The buffer that contains the data to be written to the I2C device.
+        /// The data should not include the I2C device address.</param>
+        /// <param name="readBuffer">
+        /// The buffer to read the data from the I2C device.
+        /// The length of the buffer determines how much data to read from the I2C device.
+        /// </param>
+        public void WriteRead(SpanByte writeBuffer, SpanByte readBuffer)
+        {
+            // This is allocating an intermediate buffer and then copy back the data to 
+            // the SpanByte. This is intend to be changed in a native implementation
+            byte[] toRead = new byte[readBuffer.Length];
+            _device.WriteRead(writeBuffer.ToArray(), toRead);
+            for (int i = 0; i < toRead.Length; i++)
+            {
+                readBuffer[i] = toRead[i];
+            }
+        }
+
+        /// <summary>
+        /// Creates a communications channel to a device on an I2C bus running on the current platform
+        /// </summary>
+        /// <param name="settings">The connection settings of a device on an I2C bus.</param>
+        /// <returns>A communications channel to a device on an I2C bus</returns>
+        public static I2cDevice Create(I2cConnectionSettings settings)
+        {
+            return new I2cDevice(settings);
+        }
+
+        /// <summary>
+        /// Create an I2C Device
+        /// </summary>
+        /// <param name="settings">Connection settings</param>
+        public I2cDevice(I2cConnectionSettings settings)
+        {
+            ConnectionSettings = settings;
+            _device = Windows.Devices.I2c.I2cDevice.FromId($"I2C{settings.BusId}", new Windows.Devices.I2c.I2cConnectionSettings(settings.DeviceAddress)
+            {
+                BusSpeed = (Windows.Devices.I2c.I2cBusSpeed)settings.BusSpeed,
+                SharingMode = (Windows.Devices.I2c.I2cSharingMode)settings.SharingMode
+            });
+        }
+
+        /// <inheritdoc cref="IDisposable.Dispose"/>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes this instance
+        /// </summary>
+        /// <param name="disposing"><see langword="true"/> if explicitly disposing, <see langword="false"/> if in finalizer</param>
+        void Dispose(bool disposing)
+        { }
+    }
+}

96aab044a75bf544772bb1aedcf273ebbabbc83b
José Simões <jose.simoes@eclo.solutions>
2020-10-27 18:17:14 +0000

    Add I2cTransferResult and I2cTransferStatus (#11)

--- System.Device.I2c/Properties/AssemblyInfo.cs
+++ System.Device.I2c/Properties/AssemblyInfo.cs
@@ -10,18 +10,17 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSharp.BlankApplication")]
+[assembly: AssemblyTitle("nanoFramework System.Device.I2c")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSharp.BlankApplication")]
+[assembly: AssemblyCompany(".NET nanoFramework contributors")]
+[assembly: AssemblyProduct(".NET nanoFramework System.Device.I2c")]
 [assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.2.0.2")]
+[assembly: AssemblyNativeVersion("100.0.0.1")]
 ////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
